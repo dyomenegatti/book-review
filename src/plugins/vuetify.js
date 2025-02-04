3d8aa609e6596e1f1165d1fa027fa2648157809c
Dyovana <dyo.menegatti@hotmail.com>
2025-02-04 20:57:33 -0300

    Adiciona e configura vuetify para a aplicacao

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,20 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    options: {
+      customProperties: true, 
+    },
+    dark: false, 
+    themes: {
+      light: {
+        primary: '#1D1B1B', 
+        secondary: '#F5F6FA', 
+      },
+      dark: {
+        primary: '#F5F6FA', 
+        secondary: '#1D1B1B', 
+      },
+    },
+  },
 });
